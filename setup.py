9780fd90046d37c43516d66b6959f201fc7d4a77
David Danko <dcdanko@gmail.com>
2019-01-04 16:00:26 -0500

    clean setup

--- setup.py
+++ setup.py
@@ -13,19 +13,15 @@ setuptools.setup(
     name="minerva_deconvolve",
     version="1.0.0",
     url="https://github.com/dcdanko/minerva_barcode_deconvolution",
-
     author="David C. Danko",
     author_email="dcd3001@med.cornell.edu",
-
-    description="Toolsets for deconvolving and clustering barcoded short reads",
-    long_description=open('README.rst').read(),
-
+    description="Algorithm for deconvolving and clustering barcoded short reads",
     packages=setuptools.find_packages(),
     package_dir={'minerva': 'minerva'},
-    
-    ext_modules = [setuptools.Extension("cseqs", ["cext_minerva/_cseqs.c","cext_minerva/cseqs.c"])],
-    
-    entry_points = {
+    ext_modules=[
+        setuptools.Extension("cseqs", ["cext_minerva/_cseqs.c", "cext_minerva/cseqs.c"])
+    ],
+    entry_points={
         'console_scripts': [
             'minerva_deconvolve=minerva.deconvolution.deconvolve_barcodes:main',
             'minerva_enhance_kraken=minerva.kraken.enhance_kraken:main',
@@ -33,9 +29,7 @@ setuptools.setup(
             'minerva_annotate=minerva.eval.annotate_fastq:main',
         ]
     },
-    
     install_requires=requirements,
-
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python :: 3',
